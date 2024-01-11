8f26366989bc3860762741a6a2d522a7bed02ed8
Somil Saxena <somilsaxena100@gmail.com>
2024-01-11 18:16:41 +0530

    initial commit

---
+++ functions.js
@@ -0,0 +1,19 @@
+function onePlusAvg(x,y){
+    return 1 + (x + y) /  2
+}
+let a=1;
+let b=2;
+let c=3;
+
+console.log("One plus Average of a and b is ", onePlusAvg(a,b))
+console.log("One plus Average of b and c is ", onePlusAvg(b,c))
+console.log("One plus Average of a and c is ", onePlusAvg(a,c))
+
+
+//Arrow Functions
+const hello = () =>{
+    console.log("Hey how are you")
+    return "hi"
+}
+let v=hello();
+console.log(v)
