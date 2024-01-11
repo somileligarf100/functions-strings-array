921d7c3053f59ef9d2899d4ff4b3007a96dacc4e
Somil Saxena <somilsaxena100@gmail.com>
2024-01-11 18:24:05 +0530

    initial commit

---
+++ strings.js
@@ -0,0 +1,14 @@
+let n = "Rahul"
+console.log(n)
+console.log(n.length)
+console.log(n.toUpperCase())
+console.log(n.toLowerCase())
+console.log(n.slice(2,4))
+console.log(n.replace("Ra","Le"))
+let m = "KL"
+console.log(n.concat(" is a friend of ", m))
+
+let boy1= "Pramod";
+let boy2= "Nikhil";
+let sentence= `${boy2} is a friend of ${boy1}`
+console.log(sentence)
